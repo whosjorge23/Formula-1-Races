0cb2b534e88deedd6fe7f09a6c026f1c87b8978e
Giorgio Giannotta <jorge_upa@msn.com>
2023-03-04 09:13:05 +0100

    Initial Commit

---
+++ Formula 1 Races/Formula_1_RacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formula_1_RacesApp.swift
+//  Formula 1 Races
+//
+//  Created by Giorgio Giannotta on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Formula_1_RacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
