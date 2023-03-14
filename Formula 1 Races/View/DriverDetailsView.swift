27ad27387a5905409c549484709d044094160e61
Giorgio Giannotta <jorge_upa@msn.com>
2023-03-14 20:17:16 +0100

    Implement driverPic function into DriverDetailsView

--- Formula 1 Races/View/DriverDetailsView.swift
+++ Formula 1 Races/View/DriverDetailsView.swift
@@ -14,18 +14,48 @@ struct DriverDetailsView: View {
         var body: some View {
             if let driver = viewModel.driver.first {
                 VStack {
-                    Text("\(driver.permanentNumber)")
-                        .font(.largeTitle)
-                    Text("\(driver.givenName) \(driver.familyName)")
-                        .font(.largeTitle)
-                    Text("\(driver.code)")
-                        .font(.largeTitle)
-                    Text("\(driver.nationality) \(viewModel.driverNationalityFlag(nationality: driver.nationality))")
-                        .font(.largeTitle)
-                    Text("\(driver.dateOfBirth)")
-                        .font(.largeTitle)
+                    AsyncImage(url: URL(string: viewModel.driversPic(driverID: selectedDriver))) { image in
+                                    image.resizable()
+                                        .transition(.opacity)
+                                    
+                                } placeholder: {
+                                    Image(systemName: "icloud.slash")
+                                }
+                                .frame(width: .infinity, height: 360)
+                                .overlay {
+                                    VStack{
+                                        Spacer()
+                                        Rectangle()
+                                            .frame(width: .infinity,height: 140)
+                                            .foregroundColor(Color.black.opacity(0.8))
+                                            .overlay {
+                                                VStack{
+                                                    Text("**\(driver.code)**")
+                                                        .font(.title)
+                                                    Text("\(driver.givenName) \(driver.familyName) \(driver.permanentNumber)")
+                                                        .font(.title2)
+                                                    Text("\(driver.nationality) \(viewModel.driverNationalityFlag(nationality: driver.nationality))")
+                                                        .font(.title2)
+                                                    Text("\(driver.dateOfBirth)")
+                                                        .font(.title2)
+                                                }
+                                            }
+                                        
+                                    }
+                                }
+//                    Text("\(driver.permanentNumber)")
+//                        .font(.largeTitle)
+//                    Text("\(driver.givenName) \(driver.familyName)")
+//                        .font(.largeTitle)
+//                    Text("\(driver.code)")
+//                        .font(.largeTitle)
+//                    Text("\(driver.nationality) \(viewModel.driverNationalityFlag(nationality: driver.nationality))")
+//                        .font(.largeTitle)
+//                    Text("\(driver.dateOfBirth)")
+//                        .font(.largeTitle)
+                    Spacer()
                 }
-                
+                .preferredColorScheme(.dark)
                 .onAppear {
                     viewModel.fetchDriver(driverID: selectedDriver)
                 }
