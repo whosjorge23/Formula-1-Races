8e5e622dfd08f9c2773cd84bf47edf7e8a92c20d
Giorgio Giannotta <jorge_upa@msn.com>
2023-03-25 10:25:21 +0100

    Add ResultViewModel

---
+++ Formula 1 Races/ViewModel/ResultViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ResultViewModel.swift
+//  Formula 1 Races
+//
+//  Created by Giorgio Giannotta on 25/03/23.
+//
+
+import SwiftUI
+
+class ResultViewModel: ObservableObject {
+    @Published var races = [RaceResult]()
+    
+    private let api = APIManager()
+    
+    func fetchRacesResult() {
+        api.fetchData(url: "https://ergast.com/api/f1/2023/results.json", model: ResultData.self) { result in
+            DispatchQueue.main.async {
+                self.races = result.MRData.RaceTable.Races
+            }
+        } failure: { error in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                self.fetchRacesResult()
+            }
+        }
+    }
+    
+}
